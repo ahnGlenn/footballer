47749c723a8f390f76d9b9de3d02784340654742
ansangja@gmail.com <ansangja@gmail.com>
2023-08-15 18:01:16 +0900

    footBallers Start!

---
+++ src/main/java/com/footballers/footBallers/FootBallersApplication.java
@@ -0,0 +1,14 @@
+package com.footballers.footBallers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootBallersApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FootBallersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/footballers/footBallers/FootBallersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballers.footBallers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootBallersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
